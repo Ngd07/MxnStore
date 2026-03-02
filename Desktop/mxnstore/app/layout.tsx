31928e209394d54f5447b3457763e1ee11d1d74e
Ngd07 <nleonelli2@gmail.com>
2026-03-01 22:44:52 -0300

    fix: remove Vercel Analytics to fix error

--- Desktop/mxnstore/app/layout.tsx
+++ Desktop/mxnstore/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import { I18nProvider } from '@/lib/i18n'
 import './globals.css'
 
@@ -39,7 +38,6 @@ export default function RootLayout({
         <I18nProvider>
           {children}
         </I18nProvider>
-        <Analytics />
       </body>
     </html>
   )
