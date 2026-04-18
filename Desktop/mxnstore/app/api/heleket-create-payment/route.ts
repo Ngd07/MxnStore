e21af801caf05891dcbf5fdc5c1fd561b23e8596
Ngd07 <nleonelli2@gmail.com>
2026-04-18 16:00:42 -0300

    Use Payment API key

--- Desktop/mxnstore/app/api/heleket-create-payment/route.ts
+++ Desktop/mxnstore/app/api/heleket-create-payment/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import crypto from 'crypto'
 
-const HELENET_API_KEY = '0OZYVsNwF1Qn4hDJyhJy4ER2tgKkxZqADM79GfwkQOPBaUL6JD3teIe4DDy7eyjxm1Lu3HDZqoSpKPK9N7zVz3RTrsPHC3TDStcn3zBTBitTq5ByWOBqJGXj6aw8WFmR'
+const HELENET_API_KEY = 'Gt9u65554GqmTY5d6MVCDjSkfjRrZInxy6H4jF4yMvm7SvHVRLO8ex30fnkNS2DOozfouchQfcdGD0m7MaSTsOhf6RVVEueiRdU8ruyLjjEoJ9pgLWZnYAMsbbBjGf4B'
 const HELENET_MERCHANT_ID = 'ba7a4e30-4ed2-4290-924c-3d6c885fa984'
 
 console.log('Using hardcoded credentials')
