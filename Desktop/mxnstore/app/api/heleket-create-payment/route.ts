8a4475f29c28d6120b91842f3164b13b4bcd2e77
Ngd07 <nleonelli2@gmail.com>
2026-04-18 15:48:26 -0300

    Update to correct merchant ID

--- Desktop/mxnstore/app/api/heleket-create-payment/route.ts
+++ Desktop/mxnstore/app/api/heleket-create-payment/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import crypto from 'crypto'
 
 const HELENET_API_KEY = '0OZYVsNwF1Qn4hDJyhJy4ER2tgKkxZqADM79GfwkQOPBaUL6JD3teIe4DDy7eyjxm1Lu3HDZqoSpKPK9N7zVz3RTrsPHC3TDStcn3zBTBitTq5ByWOBqJGXj6aw8WFmR'
-const HELENET_MERCHANT_ID = '2b55e311-8cbb-4f6b-b9ae-a1d9585fbb08'
+const HELENET_MERCHANT_ID = 'ba7a4e30-4ed2-4290-924c-3d6c885fa984'
 
 console.log('Using hardcoded credentials')
 
